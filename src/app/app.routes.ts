bf9fbe8d4f1df07eebc44bc2c7d2b0168b12efae
Javier Marquinez <emarquinez@gmail.com>
2018-03-28 17:47:37 +0200

    modules modifications

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,16 +8,6 @@ import { Graficas1Component } from './pages/graficas1/graficas1.component';
 import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
 
 const appRoutes: Routes = [
-  {
-    path: '',
-    component: PagesComponent,
-    children: [
-      { path: 'dasboard', component: DashboardComponent },
-      { path: 'progress', component: ProgressComponent },
-      { path: 'graficas1', component: Graficas1Component },
-      { path: '', redirectTo: '/dasboard', pathMatch: 'full' }
-    ]
-  },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: '**', component: NopagefoundComponent }
